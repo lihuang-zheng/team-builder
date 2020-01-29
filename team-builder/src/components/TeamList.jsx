8100eca9fb9f580dd0ff61c7568bb6dc9d9a4807
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-29 15:06:47 -0600

    coded the teamlist jsx.

---
+++ team-builder/src/components/TeamList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Team = props => {
+  return (
+    <div className="member-list">
+      {props.team.map(member => (
+        <div className="member" key={member.id}>
+          <h2>{member.name}</h2>
+          <p>{member.email}</p>
+          <p>{member.role}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
